80a68bbd024275bbe0e805f88b7f25792c8d7f56
monawasensei@gmail.com <monawasensei@gmail.com>
2022-04-02 16:52:13 +0000

    Built out a stub Main window and run function
    FossilOrigin-Name: 77bf9d1205ee89cfcaac1aebc8cb7bcf502067a86878c2de1f6c288d2b849618

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,6 @@
 module Main where
 
+import qualified Window.Main
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = Window.Main.runWindow
--- app/Window/Main.hs
+++ app/Window/Main.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 {-# HLINT ignore "Use newtype instead of data" #-}
 
-module Window.Main () where
+module Window.Main
+  ( runWindow,
+  )
+where
 
 import Control.Lens
 import Window.Core
@@ -18,3 +22,36 @@ data AppModel = AppModel
 data AppEvent = AppInit deriving (Show, Eq)
 
 makeLensesWith abbreviatedFields ''AppModel
+
+handleEvent ::
+  WidgetEnv AppModel AppEvent ->
+  WidgetNode AppModel AppEvent ->
+  AppModel ->
+  AppEvent ->
+  [AppEventResponse AppModel AppEvent]
+handleEvent wenv node model evt =
+  case evt of
+    AppInit -> []
+
+winConfig :: [AppConfig AppEvent]
+winConfig =
+  [ appWindowTitle "Tagger Main",
+    appInitEvent AppInit
+  ]
+    ++ themeConfig
+
+buildUI ::
+  WidgetEnv AppModel AppEvent ->
+  AppModel ->
+  WidgetNode AppModel AppEvent
+buildUI wenv model = widgetTree
+  where
+    widgetTree =
+      vstack
+        [ label "Tagger Main Window",
+          spacer
+        ]
+        `styleBasic` [padding 10]
+
+runWindow :: IO ()
+runWindow = startApp (AppModel []) handleEvent buildUI winConfig
