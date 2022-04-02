022a5560ff279fe43736323300588ab31442b50d
monawasensei@gmail.com <monawasensei@gmail.com>
2022-04-02 16:43:20 +0000

    Building a basic AppModel for the main window
    FossilOrigin-Name: 26a257868ba5d75277b5eb2d18906602751a1d8b0e5e3b5ffff373c419630501

--- app/Window/Core.hs
+++ app/Window/Core.hs
@@ -1,11 +1,14 @@
-module Window.Core (
-  module Config.Window,
-  module Data.Text,
-  module Monomer,
-  module Monomer.Common.Lens
-) where
+module Window.Core
+  ( module Config.Window,
+    module Data.Text,
+    module Monomer,
+    module Monomer.Common.Lens,
+    module Control.Lens,
+  )
+where
 
 import Config.Window
+import Control.Lens hiding (cons, index, snoc, uncons, unsnoc)
 import Data.Text
 import Monomer
 import Monomer.Common.Lens
--- app/Window/Main.hs
+++ app/Window/Main.hs
@@ -1,3 +1,20 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use newtype instead of data" #-}
+
 module Window.Main () where
 
+import Control.Lens
 import Window.Core
+
+data AppModel = AppModel
+  { _selectionFiles :: [Text]
+  }
+  deriving (Show, Eq)
+
+data AppEvent = AppInit deriving (Show, Eq)
+
+makeLensesWith abbreviatedFields ''AppModel
