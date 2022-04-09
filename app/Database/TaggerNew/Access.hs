40a391f7ece1e3936ab49eaaff86cb7bdab33323
monawasensei@gmail.com <monawasensei@gmail.com>
2022-04-09 14:30:34 +0000

    Reimplemented relate and tagFileWithDescriptor
    Using the same logic as Database.Tagger.Access.relate
    
    FossilOrigin-Name: daab9e2940a237d01ab4b4203d29855175682f1a8cd62481123dfeaae68ed65d

--- app/Database/TaggerNew/Access.hs
+++ app/Database/TaggerNew/Access.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {-# OPTIONS_GHC -Wno-typed-holes #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Redundant return" #-}
 
 module Database.TaggerNew.Access () where
 
@@ -31,6 +34,88 @@ addFile c f = do
         [validatedPath]
   insertedId <- lift . lastInsertRowId $ c
   return . File (fromIntegral insertedId) $ validatedPath
+
+tagFileWithDescriptor :: Connection -> File -> Descriptor -> IO ()
+tagFileWithDescriptor c f d =
+  execute
+    c
+    "INSERT INTO Tag (fileTagId, descriptorTagId) VALUES (?,?)"
+    (fileId f, descriptorId d)
+
+-- | Creates a relation iff these criteria are met:
+--
+-- - The InfraDescriptor is not present in the tree that is meta to the MetaDescriptor.
+--  To avoid recursive relations
+--
+-- - The InfraDescriptor is not Infra to any other Descriptor
+--
+-- With the exception of being Infra to \#UNRELATED\#
+--  - If it is related to \#UNRELATED\# This relationship is deleted before checking for
+-- any other relationships.
+relate :: Connection -> MetaDescriptor -> MaybeT IO ()
+relate c md =
+  case md of
+    MetaDescriptor metaDK infraDK -> do
+      treeMetaToParent <- fetchMetaTree c metaDK
+      infraDescriptor <- getDescriptor c infraDK
+      unless
+        (infraDescriptor `descriptorTreeElem` treeMetaToParent)
+        ( do
+            unrelatedMetaDescriptor <-
+              hoistMaybe . head'
+                <=< lift . lookupDescriptorPattern c
+                $ "#UNRELATED#"
+            let unrelatedRelation =
+                  MetaDescriptor (descriptorId unrelatedMetaDescriptor) infraDK
+            isRelatedToUnrelated <-
+              lift
+                . hasSpecificRelation c
+                $ unrelatedRelation
+            when
+              isRelatedToUnrelated
+              (lift . deleteRelation c $ unrelatedRelation)
+            hasAnyAdditionalRelations <- lift . isInfraToAny c $ infraDK
+            unless
+              hasAnyAdditionalRelations
+              ( lift
+                  . execute
+                    c
+                    "INSERT INTO MetaDescriptor (metaDescriptorId, infraDescriptorId) \
+                    \VALUES (?,?)"
+                  $ (metaDK, infraDK)
+              )
+        )
+      return ()
+
+isInfraToAny :: Connection -> DescriptorKey -> IO Bool
+isInfraToAny c k = do
+  r <-
+    query
+      c
+      "SELECT * FROM MetaDescriptor \
+      \WHERE infraDescriptorId = ?"
+      [k] ::
+      IO [Only Int]
+  return . not . null $ r
+
+hasSpecificRelation :: Connection -> MetaDescriptor -> IO Bool
+hasSpecificRelation c (MetaDescriptor mdk idk) = do
+  r <-
+    query
+      c
+      "SELECT * FROM MetaDescriptor \
+      \WHERE metaDescriptorId = ? AND infraDescriptorId = ?"
+      (mdk, idk) ::
+      IO [(Int, Int)]
+  return . not . null $ r
+
+deleteRelation :: Connection -> MetaDescriptor -> IO ()
+deleteRelation c (MetaDescriptor mdk idk) =
+  execute
+    c
+    "DELETE FROM MetaDescriptor \
+    \metaDescriptorId = ? AND infraDescriptorId = ?"
+    (mdk, idk)
 
 fetchMetaTree :: Connection -> DescriptorKey -> MaybeT IO DescriptorTree
 fetchMetaTree c pid = do
@@ -149,7 +234,7 @@ lookupFileWithTagsByFileId c fid = do
       [fid]
   return . groupRFWT [] $ r
 
-lookupDescriptorPattern :: (ToField a) => Connection -> a -> IO [Descriptor]
+lookupDescriptorPattern :: Connection -> T.Text -> IO [Descriptor]
 lookupDescriptorPattern conn p = do
   r <-
     query
