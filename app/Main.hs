1b1cd1fb1346aee5864472ec46edfdc6ef87bbf9
monawasensei@gmail.com <monawasensei@gmail.com>
2022-04-15 03:01:08 +0000

    Got some basic configuration parsing reading and validating to work in main
    FossilOrigin-Name: 5ea120ea83c17e79393d2be07a181d8ac7263e2cb3435074e0847d37a34c8e3c

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-typed-holes #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Redundant return" #-}
+{-# HLINT ignore "Use <&>" #-}
 
 module Main where
 
+import Control.Applicative (Alternative (empty))
 import Control.Lens ((^.))
+import Control.Monad
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Maybe
+import Data.Maybe
+import qualified Data.Text as T
 import Database.SQLite.Simple (Connection, close, execute_, open)
 import Database.Tagger.Access (Connection)
 import Event.Handler (taggerEventHandler)
@@ -26,6 +37,10 @@ import Node.Application
     queryAndTagEntryWidget,
     themeConfig,
   )
+import System.Directory
+import System.IO
+import Toml
+import Type.Config
 import Type.Model
   ( HasDescriptorTree (descriptorTree),
     TaggerEvent (DebugPrintSelection, TaggerInit),
@@ -70,9 +85,82 @@ runTaggerWindow c =
     taggerApplicationUI
     taggerApplicationConfig
 
+type ConfigException = String
+
+maybeAppend :: Maybe T.Text -> T.Text -> T.Text
+maybeAppend x y = maybe y (`T.append` y) x
+
+validatePath :: FilePath -> ExceptT ConfigException IO FilePath
+validatePath p = do
+  isFile <- lift . doesFileExist $ p
+  if isFile then lift . makeAbsolute $ p else throwE $ "File not found: " ++ p
+
+fileNotFoundError = (++) "File not found: "
+
+dirNotFoundError = (++) "Directory not found: "
+
+validateDir :: FilePath -> ExceptT ConfigException IO FilePath
+validateDir p = do
+  isDir <- lift . doesDirectoryExist $ p
+  if isDir then lift . makeAbsolute $ p else throwE $ "Directory not found: " ++ p
+
+getConfig :: String -> ExceptT String IO TaggerConfig
+getConfig =
+  withExceptT
+    (T.unpack . prettyTomlDecodeErrors)
+    . except
+    <=< decodeFileEither taggerConfigCodec
+    <=< validatePath
+
+getConfigDbConn :: FilePath -> ExceptT ConfigException IO FilePath
+getConfigDbConn =
+  withExceptT
+    ("Configuration error when opening database connection:\n" ++)
+    . validatePath
+
+validateConfig :: TaggerConfig -> ExceptT ConfigException IO TaggerConfig
+validateConfig (TaggerConfig dbc sc) = do
+  validatedDatabaseConfig <- validateDatabaseConfig dbc
+  validatedStyleConfig <- validateStyleConfig sc
+  withExceptT ("Error validating the configuration file:\n" ++)
+    . return
+    $ TaggerConfig validatedDatabaseConfig validatedStyleConfig
+  where
+    validateDatabaseConfig (TaggerDatabaseConfig dbpc) =
+      validateDatabasePathConfig dbpc >>= return . TaggerDatabaseConfig
+    validateDatabasePathConfig (TaggerDatabasePathConfig d db b s) = do
+      dbDir <- maybe (return "") (validateDir . T.unpack) d
+      let inDir = fmap T.pack . validatePath . (++) dbDir . T.unpack
+      dbp <- inDir db
+      bp <- inDir b
+      sp <- inDir s
+      withExceptT ("Error validating database paths:\n" ++) . return $
+        TaggerDatabasePathConfig (Just . T.pack $ dbDir) dbp bp sp
+    validateStyleConfig (TaggerStyleConfig sfc) =
+      validateStyleFontConfig sfc >>= return . TaggerStyleConfig
+    validateStyleFontConfig (TaggerStyleFontConfig d t r b) = do
+      styleFontDir <- maybe (return "") (validateDir . T.unpack) d
+      let inDir = fmap T.pack . validatePath . (++) styleFontDir . T.unpack
+      thinPath <- inDir t
+      regPath <- inDir r
+      boldPath <- inDir b
+      withExceptT ("Error validating font paths:\n" ++) . return $
+        TaggerStyleFontConfig (Just . T.pack $ styleFontDir) thinPath regPath boldPath
+
 main :: IO ()
 main = do
-  dbConn <- open "/home/monax/Repo/Haskell/tagger/images.db"
-  execute_ dbConn "PRAGMA foreign_keys = on"
-  runTaggerWindow dbConn
-  close dbConn
+  try' (getConfig "/home/monax/.config/tagger_config.toml" >>= validateConfig) $
+    \config -> do
+      dbConn <-
+        open
+          . T.unpack
+          . databasePathDatabase
+          . databasePathConfig
+          . databaseConfig
+          $ config
+      execute_ dbConn "PRAGMA foreign_keys = on"
+      runTaggerWindow dbConn
+      close dbConn
+  where
+    putEx = hPutStrLn stderr
+    try' e c = runExceptT e >>= either putEx c
--- app/Type/Config.hs
+++ app/Type/Config.hs
@@ -1,10 +1,16 @@
+{-# HLINT ignore "Use newtype instead of data" #-}
+{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
-{-# HLINT ignore "Use newtype instead of data" #-}
-
 module Type.Config
   ( TaggerConfig (..),
+    OptAbsPath (..),
+    TaggerDatabaseConfig (..),
+    TaggerDatabasePathConfig (..),
+    TaggerStyleConfig (..),
+    TaggerStyleFontConfig (..),
+    taggerConfigCodec,
   )
 where
 
@@ -41,7 +47,7 @@ data TaggerConfig = TaggerConfig
   { databaseConfig :: !TaggerDatabaseConfig,
     styleConfig :: !TaggerStyleConfig
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, OptAbsPath)
 
 taggerConfigCodec :: Toml.TomlCodec TaggerConfig
 taggerConfigCodec =
@@ -52,7 +58,7 @@ taggerConfigCodec =
 data TaggerStyleConfig = TaggerStyleConfig
   { styleFontConfig :: !TaggerStyleFontConfig
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, OptAbsPath)
 
 taggerStyleConfigCodec :: Toml.TomlCodec TaggerStyleConfig
 taggerStyleConfigCodec =
@@ -67,6 +73,9 @@ data TaggerStyleFontConfig = TaggerStyleFontConfig
   }
   deriving (Show, Eq)
 
+instance OptAbsPath TaggerStyleFontConfig where
+  getResolvedPath pf c = maybe (pf c) (T.append (pf c)) (fontDirectory c)
+
 taggerStyleFontConfigCodec :: Toml.TomlCodec TaggerStyleFontConfig
 taggerStyleFontConfigCodec =
   TaggerStyleFontConfig
@@ -78,7 +87,7 @@ taggerStyleFontConfigCodec =
 data TaggerDatabaseConfig = TaggerDatabaseConfig
   { databasePathConfig :: !TaggerDatabasePathConfig
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, OptAbsPath)
 
 taggerDatabaseConfigCodec :: Toml.TomlCodec TaggerDatabaseConfig
 taggerDatabaseConfigCodec =
@@ -93,6 +102,9 @@ data TaggerDatabasePathConfig = TaggerDatabasePathConfig
   }
   deriving (Show, Eq)
 
+instance OptAbsPath TaggerDatabasePathConfig where
+  getResolvedPath pf c = maybe (pf c) (T.append (pf c)) (databasePathDirectory c)
+
 taggerDatabasePathConfigCodec :: Toml.TomlCodec TaggerDatabasePathConfig
 taggerDatabasePathConfigCodec =
   TaggerDatabasePathConfig
@@ -100,3 +112,7 @@ taggerDatabasePathConfigCodec =
     <*> Toml.text "database" .= databasePathDatabase
     <*> Toml.text "backup" .= databasePathBackup
     <*> Toml.text "script" .= databasePathScript
+
+class OptAbsPath a where
+  getResolvedPath :: (a -> T.Text) -> a -> T.Text
+  getResolvedPath pf c = pf c
