ac91e4a0f3ab859dc71c80e995032115ac0d23c2
monawasensei@gmail.com <monawasensei@gmail.com>
2022-05-13 18:04:19 +0000

    Created view definition called FileWithTags for, hopefully, easier querying of tags, subtags, and untagged
    FossilOrigin-Name: 8befb096816ffd0553d272a504ed3807f9ba02c9e279cb9671d70af101fd77cc

--- taggerSchemaDef.sql
+++ taggerSchemaDef.sql
@@ -34,6 +34,74 @@ CREATE TABLE IF NOT EXISTS "Representative" (
   FOREIGN KEY("repDescriptorId") REFERENCES "Descriptor"("id") ON DELETE CASCADE,
   CONSTRAINT "repDescriptorUnique" UNIQUE("repDescriptorId") ON CONFLICT REPLACE
 );
+CREATE VIEW IF NOT EXISTS FileWithTags (
+  mainTagId, 
+  mainTagFileId, 
+  mainTagFilePath, 
+  mainTagDescriptorId, 
+  mainTagDescriptor,
+  mainTagSubTagOfId, 
+  subTagId, 
+  subTagFileId, 
+  subTagFilePath, 
+  subTagDescriptorId, 
+  subTagDescriptor,
+  subTagSubTagOfId
+) AS
+
+WITH FWT AS 
+(SELECT 
+  t1.id,
+  f1.id "fileTagId",
+  f1.filePath,
+  d1.id "descriptorTagId",
+  d1.descriptor,
+  t1.subTagOfId
+FROM Tag t1
+JOIN File f1
+ON t1.fileTagId = f1.id
+JOIN Descriptor d1
+ON t1.descriptorTagId = d1.id
+)
+
+SELECT 
+  mainTag.id "mainTagId",
+  mainTag.fileTagId "mainTagFileId",
+  mainTag.filePath "mainTagFilePath",
+  mainTag.descriptorTagId "mainTagDescriptorId",
+  mainTag.descriptor "mainTagDescriptor",
+  mainTag.subTagOfId "mainTagSubTagOfId",
+  subTag.id "subTagId",
+  subTag.fileTagId "subTagFileId",
+  subTag.filePath "subTagFilePath",
+  subTag.descriptorTagId "subTagDescriptorId",
+  subTag.descriptor "subTagDescriptor",
+  subTag.subTagOfId "subTagSubTagOfId"
+FROM FWT mainTag
+LEFT JOIN FWT subTag
+ON mainTag.id = subTag.subTagOfId
+
+UNION
+SELECT 
+  NULL,
+  f.id,
+  f.filePath,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL
+FROM File f
+LEFT JOIN Tag t
+ON f.id = t.fileTagId
+WHERE t.id IS NULL
+
+ORDER BY mainTag.filePath
+;
 INSERT INTO Descriptor (descriptor) VALUES ('#ALL#'), ('#META#'),  ('#UNRELATED#');
 -- relate #META# and #UNRELATED# to #ALL#
 INSERT INTO MetaDescriptor (metaDescriptorId, infraDescriptorId)
