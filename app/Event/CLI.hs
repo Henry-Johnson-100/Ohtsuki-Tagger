359b240e3cb26a9ede36ec5b6f11f9bfda84dc89
monawasensei@gmail.com <monawasensei@gmail.com>
2022-05-24 21:58:43 +0000

    Moved functions from Event.CLI.Type to Event.CLI
    FossilOrigin-Name: 36180731ec9dc33a8dfa0e6758ac011ac2456c6e2ae4f9cb2b32e2190ce41739

--- app/Event/CLI.hs
+++ app/Event/CLI.hs
@@ -1,9 +1,16 @@
 {-# OPTIONS_GHC -Wno-typed-holes #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use lambda-case" #-}
 
 module Event.CLI
   ( module Event.CLI.Type,
     showOptErrors,
     cliQuery,
+    getTaggerOpt,
+    nullOpts,
+    hasQueryFlag,
+    getQuery,
   )
 where
 
@@ -42,3 +49,26 @@ cliQuery c qs = do
   r <- runQuery c Union ByTag emptyBufferList q
   let fs = map (filePath . file) . cCollect $ r
   mapM_ T.IO.putStrLn fs
+
+getTaggerOpt :: [String] -> TaggerOpts
+getTaggerOpt = uncurryOpts . IO.getOpt IO.RequireOrder cliFlags
+  where
+    uncurryOpts :: ([CLIFlag], [String], [String]) -> TaggerOpts
+    uncurryOpts (cs, ns, es) = TaggerOpts cs ns es
+
+nullOpts :: TaggerOpts -> Bool
+nullOpts (TaggerOpts [] [] []) = True
+nullOpts _ = False
+
+hasQueryFlag :: TaggerOpts -> Bool
+hasQueryFlag = any (\f -> case f of Query _ -> True; _ -> False) . optionArguments
+
+getQuery :: TaggerOpts -> Maybe String
+getQuery opts =
+  if not . hasQueryFlag $ opts
+    then Nothing
+    else getQuery' . optionArguments $ opts
+  where
+    getQuery' [] = Nothing
+    getQuery' (Query s : _) = Just s
+    getQuery' (_ : xs) = getQuery' xs
--- app/Event/CLI/Type.hs
+++ app/Event/CLI/Type.hs
@@ -6,10 +6,7 @@
 module Event.CLI.Type
   ( CLIFlag (..),
     TaggerOpts (..),
-    getTaggerOpt,
-    nullOpts,
-    hasQueryFlag,
-    getQuery,
+    cliFlags,
   )
 where
 
@@ -27,26 +24,6 @@ data TaggerOpts = TaggerOpts
     optionErrors :: ![String]
   }
   deriving (Show, Eq)
-
-nullOpts :: TaggerOpts -> Bool
-nullOpts (TaggerOpts [] [] []) = True
-nullOpts _ = False
-
-hasQueryFlag :: TaggerOpts -> Bool
-hasQueryFlag = any (\f -> case f of Query _ -> True; _ -> False) . optionArguments
-
-getQuery :: TaggerOpts -> Maybe String
-getQuery opts =
-  if not . hasQueryFlag $ opts
-    then Nothing
-    else getQuery' . optionArguments $ opts
-  where
-    getQuery' [] = Nothing
-    getQuery' (Query s : _) = Just s
-    getQuery' (_ : xs) = getQuery' xs
-
-uncurryOpts :: ([CLIFlag], [String], [String]) -> TaggerOpts
-uncurryOpts (cs, ns, es) = TaggerOpts cs ns es
 
 data CLIFlag
   = Version
@@ -69,6 +46,3 @@ cliFlags =
       \Implicit query criteria tokens default to 'Tag'.\n\
       \Ex. \"otsuki_yui {r.cute} d| r.season i| sweater\""
   ]
-
-getTaggerOpt :: [String] -> TaggerOpts
-getTaggerOpt = uncurryOpts . IO.getOpt IO.RequireOrder cliFlags
