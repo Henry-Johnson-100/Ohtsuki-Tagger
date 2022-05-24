2b528b558875ade9c53b314222aaf0941ac05963
monawasensei@gmail.com <monawasensei@gmail.com>
2022-05-24 13:50:13 +0000

    Made simple args
    FossilOrigin-Name: 69cb3e55c91f2610a9c1058215e5eb006712a87f49f0a4c542d6f8029a408813

--- app/Event/CLI/Type.hs
+++ app/Event/CLI/Type.hs
@@ -1,3 +1,34 @@
-module Event.CLI.Type () where
+{-# OPTIONS_GHC -Wno-typed-holes #-}
+
+module Event.CLI.Type
+  ( CLIFlag (..),
+    getTaggerOpt,
+  )
+where
 
 import qualified IO
+
+data CLIFlag
+  = Version
+  | Query !String
+  deriving (Show, Eq)
+
+cliFlags :: [IO.OptDescr CLIFlag]
+cliFlags =
+  [ IO.Option
+      ['v']
+      ["version"]
+      (IO.NoArg Version)
+      "Show version.",
+    IO.Option
+      ['q']
+      ["query"]
+      (IO.ReqArg Query "QUERY")
+      "Query the database using TaggerQL and \
+      \a list of file paths. \
+      \Implicit query criteria tokens default to 'Tag'.\n\
+      \Ex. \"otsuki_yui {r.cute} d| r.season i| sweater\""
+  ]
+
+getTaggerOpt :: [String] -> ([CLIFlag], [String], [String])
+getTaggerOpt = IO.getOpt IO.RequireOrder cliFlags
