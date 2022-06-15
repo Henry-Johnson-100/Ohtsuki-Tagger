cad5c0c880f200d4be552f73ff2d99dda65e4482
monax <>
2022-06-15 18:36:07 +0000

    Added some more functions to HierarchyMap
    FossilOrigin-Name: cde8fa161ef370a9435820d95278056513feaa7716c4d3a1d06f8644ebd70d31

--- lib/Data/HierarchyMap.hs
+++ lib/Data/HierarchyMap.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE StrictData #-}
 
 {- |
@@ -13,16 +14,21 @@ module Data.HierarchyMap (
   mapHierarchyMap,
   union,
   member,
+  find,
+  Data.HierarchyMap.lookup,
   metaMember,
   infraMember,
   insert,
   empty,
+  isMetaCircular,
+  getAllInfraTo,
 ) where
 
 import qualified Data.HashMap.Strict as HashMap
 import qualified Data.HashSet as HashSet
 import Data.Hashable (Hashable)
 import Data.Hierarchy.Internal
+import Data.Maybe
 
 {- |
  Map over all elements in the hierarchy.
@@ -45,6 +51,18 @@ member :: Hashable a => a -> HierarchyMap a -> Bool
 member x (HierarchyMap m) = HashMap.member x m
 
 {- |
+ Return empty set if the key is not in the map.
+-}
+find :: Hashable a => a -> HierarchyMap a -> HashSet.HashSet a
+find x (HierarchyMap m) = fromMaybe HashSet.empty $ HashMap.lookup x m
+
+{- |
+ Return Nothing if the key is not in the map.
+-}
+lookup :: Hashable k => k -> HierarchyMap k -> Maybe (HashSet.HashSet k)
+lookup x (HierarchyMap m) = HashMap.lookup x m
+
+{- |
  True if the given value has a meta relationship to any member of the map.
 
  The value must be a key in the map and have a non-null HashSet value.
@@ -57,3 +75,23 @@ metaMember x (HierarchyMap m) = maybe False (not . HashSet.null) (HashMap.lookup
 -}
 infraMember :: Hashable a => a -> HierarchyMap a -> Bool
 infraMember x (HierarchyMap m) = maybe False HashSet.null $ HashMap.lookup x m
+
+{- |
+ Returns 'True` if the given value is meta to itself at any point in the map.
+-}
+isMetaCircular :: Hashable a => a -> HierarchyMap a -> Bool
+isMetaCircular x = HashSet.member x . getAllInfraTo x
+
+{- |
+ Retrieve a set of all elements that are infra to the given value.
+
+ Does not include the given value.
+-}
+getAllInfraTo :: Hashable a => a -> HierarchyMap a -> HashSet.HashSet a
+getAllInfraTo x hm =
+  let !layerInfra = Data.HierarchyMap.find x hm
+   in HashSet.union layerInfra
+        . HashSet.unions
+        . map (`getAllInfraTo` hm)
+        . HashSet.toList
+        $ layerInfra
