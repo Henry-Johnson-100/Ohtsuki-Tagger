b426def119f5e20a08d1cecdfa6ac6ae8afe1d56
monax <>
2022-06-25 19:38:03 +0000

    Working on function implementation in Text.TaggerQL
    Implemented querySimpleTerm completely, runRequest, queryRequest, and querySentence
    
    Partially implemented queryTermTree.
    
    Still need implementations for queryCombinableSentence and queryTermRelation (which will
    require writing more SQL)
    
    FossilOrigin-Name: c06d53b77c5f476453be7d5692318f14f13b6f47bcb3a24cc9c028f246e6f76d

--- lib/Text/TaggerQL.hs
+++ lib/Text/TaggerQL.hs
@@ -1,6 +1,6 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-
 {-# HLINT ignore "Use concatMap" #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 {- |
 Module      : Text.TaggerQL
@@ -17,13 +17,18 @@ module Text.TaggerQL (
   queryRequest,
 ) where
 
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Maybe
+import qualified Data.HashSet as HS
 import qualified Data.HashSet as HashSet
 import Data.Hashable
 import qualified Data.List as L
 import qualified Data.List.NonEmpty as NE
+import Data.Maybe
 import Data.Tagger
 import qualified Data.Text as T
 import Database.Tagger
+import Tagger.Util
 import Text.TaggerQL.AST
 
 data CombinableSentenceResult
@@ -36,7 +41,7 @@ combinableSentenceResultSetOp (CombinableSentenceResult so _) = so
 combinableSentenceResultSet :: CombinableSentenceResult -> HashSet.HashSet File
 combinableSentenceResultSet (CombinableSentenceResult _ s) = s
 
-newtype TermResult = TermResult (HashSet.HashSet File) deriving (Show, Eq)
+newtype TermResult = TermResult {termResult :: HashSet.HashSet File} deriving (Show, Eq)
 
 newtype SimpleTerm = SimpleTerm (TermTree T.Text)
   deriving (Show, Eq)
@@ -57,25 +62,6 @@ queryRequest ::
   IO CombinableSentenceResult
 queryRequest tc (Request cs) = combineSentences <$> mapM (queryCombinableSentence tc) cs
 
-combineSentences :: [CombinableSentenceResult] -> CombinableSentenceResult
-combineSentences [] = emptyCombinableSentenceResult
-combineSentences cs = L.foldl1' combineSentence cs
-
-{- |
- Return an empty result with a default 'SetOp` of Union.
--}
-emptyCombinableSentenceResult :: CombinableSentenceResult
-emptyCombinableSentenceResult = undefined
-
-{- |
- Left-associative combination of the results of a combinable sentence query.
--}
-combineSentence ::
-  CombinableSentenceResult ->
-  CombinableSentenceResult ->
-  CombinableSentenceResult
-combineSentence x y = undefined
-
 queryCombinableSentence ::
   TaggedConnection ->
   CombinableSentence T.Text ->
@@ -86,13 +72,23 @@ querySentence ::
   TaggedConnection ->
   Sentence T.Text ->
   IO TermResult
-querySentence tc s = undefined
+querySentence tc (Sentence tts) =
+  intersectTermResults . catMaybes <$> mapM (runMaybeT . queryTermTree tc) tts
 
+{- |
+ Return 'Nothing` if the given tree is 'Bottom`,
+ that way, these terms can be filtered before combination instead of trying to do things
+ like intersect an empty bottom set.
+-}
 queryTermTree ::
   TaggedConnection ->
   TermTree T.Text ->
-  IO TermResult
-queryTermTree tc tt = undefined
+  MaybeT IO TermResult
+queryTermTree tc tt =
+  case tt of
+    Simple _ -> lift . querySimpleTerm tc . SimpleTerm $ tt
+    Bottom _ -> hoistMaybe Nothing
+    t :<- ts -> lift undefined
 
 queryTermRelation ::
   TaggedConnection ->
@@ -108,7 +104,47 @@ querySimpleTerm ::
   TaggedConnection ->
   SimpleTerm ->
   IO TermResult
-querySimpleTerm tc (SimpleTerm tt) = undefined
+querySimpleTerm tc (SimpleTerm (termTreeNode -> Term qc p)) =
+  case qc of
+    DescriptorCriteria ->
+      TermResult . HS.fromList <$> flatQueryForFileByTagDescriptorPattern p tc
+    MetaDescriptorCriteria ->
+      TermResult . HS.fromList <$> flatQueryForFileOnMetaRelationPattern p tc
+    FilePatternCriteria ->
+      TermResult . HS.fromList <$> queryForFileByPattern p tc
+    UntaggedCriteria ->
+      TermResult . HS.fromList <$> queryForUntaggedFiles tc
+
+{- |
+ Should perform an associative strict intersection of the given 'TermResult`s.
+-}
+intersectTermResults :: [TermResult] -> TermResult
+intersectTermResults [] = emptyTermResult
+intersectTermResults trs = TermResult . L.foldl1' HS.intersection . map termResult $ trs
+
+combineSentences :: [CombinableSentenceResult] -> CombinableSentenceResult
+combineSentences [] = emptyCombinableSentenceResult
+combineSentences cs = L.foldl1' combineSentence cs
+
+{- |
+ Left-associative combination of the results of a combinable sentence query.
+-}
+combineSentence ::
+  CombinableSentenceResult ->
+  CombinableSentenceResult ->
+  CombinableSentenceResult
+combineSentence
+  (CombinableSentenceResult sox sx)
+  (CombinableSentenceResult soy sy) = CombinableSentenceResult sox $ combFun soy sx sy
+
+{- |
+ Return an empty result with a default 'SetOp` of Union.
+-}
+emptyCombinableSentenceResult :: CombinableSentenceResult
+emptyCombinableSentenceResult = undefined
+
+emptyTermResult :: TermResult
+emptyTermResult = TermResult HS.empty
 
 combFun ::
   Hashable a =>
