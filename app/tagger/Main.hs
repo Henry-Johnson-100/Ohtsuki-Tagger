26fbfac2d4c3636b16e877cfaeac58abdccfd7c5
monax <>
2022-06-28 17:53:38 +0000

    Added some basic functions in main and the config module to parse the config and change the current working directory to the dir that contains the db file.
    FossilOrigin-Name: 8146e54fdfd3b0fb1b79b1f02c02fb75e343ea94a443f7867af0d3defd2fbbed

--- app/tagger/Main.hs
+++ app/tagger/Main.hs
@@ -1,2 +1,33 @@
+import Control.Lens
+import Control.Monad.Trans.Except
+import Data.Config
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T.IO
+import System.Directory
+import System.FilePath
+import System.IO
+
 main :: IO ()
-main = putStrLn "tagger"
+main = do
+  ec <- runExceptT getConfig
+  either
+    printConfigError
+    runWithConfig
+    ec
+
+runWithConfig :: TaggerConfig -> IO ()
+runWithConfig c = do
+  workingDir <- getCurrentDirectory
+  dbDir <- makeAbsolute . takeDirectory . T.unpack $ c ^. dbConf . path
+  setCurrentDirectory dbDir
+  getCurrentDirectory >>= putStrLn
+  setCurrentDirectory workingDir
+  getCurrentDirectory >>= putStrLn
+
+printConfigError :: Text -> IO ()
+printConfigError e = do
+  hPutStrLn stderr "Error while parsing config file:\n"
+  T.IO.hPutStrLn stderr e
+  hPutStrLn stderr "\nPlease use this as a template:\n"
+  hPrintConf stderr exampleConf
