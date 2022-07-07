4d698b0163b57edbe3f0ae06738324d5366c9c36
monax <>
2022-07-07 11:46:48 +0000

    Added test cases for the two tickets I have open at the moment.
    FossilOrigin-Name: f2f19b155fea0421d75186d35cf17eb18e214c7d507fa930d540c3424186ddbf

--- test/Main.hs
+++ test/Main.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Data.List.NonEmpty
 import Data.Tagger
 import Test.Tasty
 import Test.Tasty.HUnit
-import Text.TaggerQL
 import Text.TaggerQL.AST
 import Text.TaggerQL.Parser.Internal
 
+main :: IO ()
 main = defaultMain (testGroup "Test" [parserEdgeCases])
 
 parserEdgeCases :: TestTree
@@ -62,5 +63,78 @@ parserEdgeCases =
                 )
             )
             (parse requestParser "test" "otsuki% d|(bruh u| (squad) )")
+        )
+    , testCase
+        "Use a parenthetical query in the middle of a query"
+        ( assertEqual
+            ""
+            ( Right
+                ( Request
+                    [ SentenceNode
+                        ( CombinableSentence
+                            Intersect
+                            ( Sentence
+                                [ Simple
+                                    ( SimpleTerm
+                                        (Term MetaDescriptorCriteria "kazuho")
+                                    )
+                                ]
+                            )
+                        )
+                    , SentenceNode
+                        ( CombinableSentence
+                            Union
+                            ( Sentence
+                                [ Simple
+                                    ( SimpleTerm
+                                        (Term MetaDescriptorCriteria "kaede")
+                                    )
+                                ]
+                            )
+                        )
+                    , SentenceBranch
+                        Union
+                        [ SentenceNode
+                            ( CombinableSentence
+                                Intersect
+                                ( Sentence
+                                    [ Simple
+                                        ( SimpleTerm
+                                            (Term MetaDescriptorCriteria "%kirari")
+                                        )
+                                    , Simple
+                                        ( SimpleTerm
+                                            (Term MetaDescriptorCriteria "hold")
+                                        )
+                                    , Simple
+                                        ( SimpleTerm
+                                            (Term MetaDescriptorCriteria "f%anzu")
+                                        )
+                                    ]
+                                )
+                            )
+                        ]
+                    , SentenceNode
+                        ( CombinableSentence
+                            Union
+                            ( Sentence
+                                [ Complex
+                                    ( Term MetaDescriptorCriteria "k%akane"
+                                        :<- ( Bottom
+                                                (Term MetaDescriptorCriteria "smile")
+                                                :| []
+                                            )
+                                    )
+                                ]
+                            )
+                        )
+                    ]
+                )
+            )
+            ( parse
+                requestParser
+                "test"
+                "kazuho u| kaede u| (%kirari hold f%anzu) u| k%akane {smile}"
+            )
         )
     ]
