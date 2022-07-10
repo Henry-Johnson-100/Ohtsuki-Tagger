a6ef5483362fd11186f4e37eb04cf59769ef802b
monax <>
2022-07-10 20:13:42 +0000

    added getopts to the main module to read the database path from an option.
    FossilOrigin-Name: b8e59e120f2cd13875ad1f077684d79a48bbe602c67bbcb20c497a45d4d7e501

--- app/tagger/Main.hs
+++ app/tagger/Main.hs
@@ -1,30 +1,113 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
-import Data.Model
+{-# HLINT ignore "Use lambda-case" #-}
+
+import Control.Monad (guard, when)
+import Control.Monad.Trans.Except (
+  ExceptT,
+  runExceptT,
+  throwE,
+  withExceptT,
+ )
+import Data.Model.Core (
+  createTaggerModel,
+  focusedFileDefaultDataFile,
+ )
 import qualified Data.Text as T
-import Database.Tagger
-import Interface
-import Paths_tagger
-import System.Directory
-import System.FilePath
+import Data.Version (showVersion)
+import Database.Tagger (Descriptor (Descriptor), open)
+import Interface (runTagger)
+import Paths_tagger (getDataFileName)
+import System.Console.GetOpt (
+  ArgDescr (NoArg, ReqArg),
+  ArgOrder (ReturnInOrder),
+  OptDescr (..),
+  getOpt,
+ )
+import System.Directory (
+  getCurrentDirectory,
+  makeAbsolute,
+  setCurrentDirectory,
+ )
+import System.Environment (getArgs)
+import System.FilePath (takeDirectory)
+import System.IO (hPutStrLn, stderr)
+import Tagger.Info (taggerVersion)
+
+data ProgArg
+  = ProgVersion
+  | ProgDBPath FilePath
+  deriving (Show, Eq)
+
+options :: [OptDescr ProgArg]
+options =
+  [ Option
+      "vV"
+      ["version"]
+      (NoArg ProgVersion)
+      "Show the version."
+  , Option
+      "pP"
+      ["path"]
+      (ReqArg ProgDBPath "PATH")
+      "The path to the tagger database."
+  ]
+
+argOrder :: ArgOrder ProgArg
+argOrder = ReturnInOrder ProgDBPath
 
 main :: IO ()
-main = withConfig
+main = do
+  argString <- getArgs
+  let p@(opts, _, _) =
+        getOpt argOrder options argString
+  when
+    (ProgVersion `elem` opts)
+    (putStrLn . showVersion $ taggerVersion)
+  eitherDBPath <- runExceptT $ getConnString p
+  either (hPutStrLn stderr) withDBPath eitherDBPath
+ where
+  head' [] = Nothing
+  head' (x : _) = Just x
 
-withConfig :: IO ()
-withConfig = do
+  getConnString ::
+    ([ProgArg], [String], [String]) ->
+    ExceptT String IO ProgArg
+  getConnString (opts, nonOpts, errors) = do
+    withExceptT
+      (const ("Unknown options: " ++ unlines nonOpts))
+      (guard (null nonOpts) :: ExceptT String IO ())
+    withExceptT
+      (const ("Errors reading options: " ++ unlines errors))
+      (guard (null errors) :: ExceptT String IO ())
+    maybe
+      (throwE "No database path specified, use '-p PATH'")
+      return
+      ( head'
+          . filter
+            (\x -> case x of (ProgDBPath _) -> True; _ -> False)
+          $ opts
+      )
+
+withDBPath :: ProgArg -> IO ()
+withDBPath (ProgDBPath p) = do
   workingDir <- getCurrentDirectory
-  dbDir <- makeAbsolute . takeDirectory . T.unpack $ ""
+  dbDir <- makeAbsolute . takeDirectory $ p
   setCurrentDirectory dbDir
-  runProgram
+  runProgram p
   setCurrentDirectory workingDir
+withDBPath _ =
+  hPutStrLn
+    stderr
+    "Something went wrong with the given database path option."
 
 {- |
  Entry point for running the monomer program.
 -}
-runProgram :: IO ()
-runProgram = do
-  db <- openTaggedConnection
+runProgram :: FilePath -> IO ()
+runProgram p = do
+  db <- open p
   defaultFile <- T.pack <$> getDataFileName focusedFileDefaultDataFile
   runTagger
     ( createTaggerModel
@@ -33,6 +116,3 @@ runProgram = do
         (Descriptor (-2) "fake #UNRELATED#")
         defaultFile
     )
-
-openTaggedConnection :: IO TaggedConnection
-openTaggedConnection = open ""
