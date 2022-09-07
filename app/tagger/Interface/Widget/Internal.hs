76e529f4a018aec35dc53fb3b3cfe905586d5089
monax <>
2022-09-07 01:26:00 +0000

    Moved the detail pane widget to the new module, as a draft.
    FossilOrigin-Name: 0af4cf8893286553ac2ed9c8e365896a98923dd4e4bdab05af867d946b7f4a6a

--- app/tagger/Interface/Widget/Internal.hs
+++ app/tagger/Interface/Widget/Internal.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ViewPatterns #-}
@@ -22,8 +21,6 @@ module Interface.Widget.Internal (
 
 import Control.Lens hiding (both)
 import Data.Event
-import qualified Data.HashSet as HS
-import qualified Data.HierarchyMap as HM
 import qualified Data.List as L
 import Data.Model
 import Data.Model.Shared
@@ -104,7 +101,7 @@ focusedFileWidget m =
       ( withStyleBasic [borderR 1 black] focusedFileMainPane
       , withNodeVisible
           (not $ (m ^. visibilityModel) `hasVis` VisibilityLabel hidePossibleUIVis)
-          $ detailPane m
+          $ undefined
       )
  where
   focusedFileMainPane =
@@ -202,178 +199,6 @@ focusedFileWidget m =
 
 imagePreviewRender :: Text -> TaggerWidget
 imagePreviewRender fp = image_ fp [fitEither, alignCenter]
-
-detailPane :: TaggerModel -> TaggerWidget
-detailPane m@((^. focusedFileModel . focusedFile) -> (ConcreteTaggedFile _ hm)) =
-  hstack_
-    []
-    [ separatorLine
-    , detailPaneTagsWidget
-    ]
- where
-  detailPaneTagsWidget =
-    withStyleBasic
-      [paddingR 20]
-      $ vstack_
-        []
-        [ vstack_
-            []
-            [ filePathWidget
-            , separatorLine
-            , vscroll_ [wheelRate 50] $
-                vstack
-                  [ metaLeaves
-                      ( L.sortOn (descriptor . concreteTagDescriptor)
-                          . filter
-                            ( \x ->
-                                HM.metaMember x hm
-                                  && not (HM.infraMember x hm)
-                            )
-                          . HM.keys
-                          $ hm
-                      )
-                  , spacer_ [resizeFactor (-1)]
-                  , nullMemberLeaves
-                      ( L.sortOn (descriptor . concreteTagDescriptor)
-                          . filter
-                            ( \x ->
-                                not (HM.metaMember x hm)
-                                  && not (HM.infraMember x hm)
-                            )
-                          . HM.keys
-                          $ hm
-                      )
-                  , deleteTagZone
-                  ]
-            ]
-        , withStyleBasic [paddingT 20] $
-            vstack
-              [ separatorLine
-              , withStyleBasic [maxHeight 400] $ undefined m
-              ]
-        ]
-   where
-    filePathWidget :: TaggerWidget
-    filePathWidget =
-      hstack_
-        []
-        [ withNodeVisible
-            ( focusedFileDefaultRecordKey
-                /= (fileId . concreteTaggedFile $ m ^. focusedFileModel . focusedFile)
-            )
-            $ styledButton_
-              [resizeFactor (-1)]
-              "Rename"
-              ( DoFocusedFileEvent
-                  (ToggleFocusedFilePaneVisibility fileRenameModeVis)
-              )
-        , zstack_
-            []
-            [ withNodeVisible (not isFileRenameMode) $
-                flip
-                  label_
-                  [resizeFactor (-1)]
-                  (filePath . concreteTaggedFile $m ^. focusedFileModel . focusedFile)
-            , withNodeVisible isFileRenameMode
-                . keystroke_
-                  [
-                    ( "Enter"
-                    , DoFileSelectionEvent
-                        . RenameFile
-                        $ ( fileId . concreteTaggedFile $
-                              m ^. focusedFileModel . focusedFile
-                          )
-                    )
-                  ]
-                  []
-                $ textField_
-                  ( fileSelectionModel
-                      . fileSelectionInfoMap
-                      . fileInfoAt
-                        ( fromIntegral
-                            . fileId
-                            . concreteTaggedFile
-                            $ m ^. focusedFileModel . focusedFile
-                        )
-                      . fileInfoRenameText
-                  )
-                  []
-            ]
-        ]
-     where
-      fileRenameModeVis = "file-rename"
-      isFileRenameMode =
-        (m ^. focusedFileModel . focusedFileVis)
-          `hasVis` VisibilityLabel fileRenameModeVis
-    nullMemberLeaves members =
-      withStyleBasic [borderB 1 black]
-        . vstack_ []
-        $ ( \ct@(ConcreteTag tk (Descriptor _ dp) _) ->
-              subTagDropTarget tk
-                . box_ [alignLeft, alignTop]
-                . draggable ct
-                $ label dp
-          )
-          <$> members
-    metaLeaves :: [ConcreteTag] -> TaggerWidget
-    metaLeaves members =
-      vstack_ [] . L.intersperse spacer $
-        (flip metaLeaf hm <$> members)
-     where
-      metaLeaf l@(ConcreteTag tk (Descriptor _ dp) _) hmap =
-        let subtags =
-              L.sortOn (descriptor . concreteTagDescriptor)
-                . HS.toList
-                $ HM.find l hmap
-         in if null subtags
-              then
-                subTagDropTarget tk . box_ [alignLeft, alignTop]
-                  . draggable l
-                  $ label dp
-              else
-                vstack_
-                  []
-                  [ hstack_
-                      []
-                      [ subTagDropTarget tk
-                          . box_ [alignLeft, alignTop]
-                          . draggable l
-                          . withStyleBasic [textColor yuiBlue]
-                          $ label dp
-                      , spacer
-                      , label "{"
-                      ]
-                  , hstack_
-                      []
-                      [ metaTagLeafSpacer
-                      , box_ [alignLeft, alignTop] $
-                          vstack
-                            ( flip metaLeaf hmap
-                                <$> subtags
-                            )
-                      ]
-                  , label "}"
-                  ]
-       where
-        metaTagLeafSpacer = spacer_ [width 20]
-    deleteTagZone :: TaggerWidget
-    deleteTagZone =
-      dropTarget_
-        (DoFocusedFileEvent . DeleteTag . concreteTagId)
-        [dropTargetStyle [border 1 yuiRed]]
-        . flip styleHoverSet []
-        . withStyleBasic [bgColor yuiLightPeach, border 1 yuiPeach]
-        $ buttonD_ "Delete" [resizeFactor (-1)]
-    subTagDropTarget tk =
-      dropTarget_
-        (\(Descriptor dk _) -> DoFocusedFileEvent (TagFile dk (Just tk)))
-        [dropTargetStyle [border 1 yuiBlue]]
-        . dropTarget_
-          ( \ct ->
-              DoFocusedFileEvent
-                (MoveTag ct (Just tk))
-          )
-          [dropTargetStyle [border 1 yuiRed]]
 
 tagTextField :: TaggerWidget
 tagTextField =
--- app/tagger/Interface/Widget/Internal/FileDetail.hs
+++ app/tagger/Interface/Widget/Internal/FileDetail.hs
@@ -1,7 +1,186 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Eta reduce" #-}
 
 module Interface.Widget.Internal.FileDetail (widget) where
 
+import Control.Lens
+import Data.Event
+import qualified Data.HashSet as HS
+import qualified Data.HierarchyMap as HM
+import qualified Data.List as L
+import Data.Model
+import Data.Model.Shared
+import Database.Tagger
+import Interface.Theme
+import Interface.Widget.Internal.Core
+import Interface.Widget.Internal.Type
 import Monomer
 
-widget _ = label_ "Here is the file Detail" [resizeFactor (-1)]
+widget :: TaggerModel -> TaggerWidget
+widget m = detailPane m
+
+detailPane :: TaggerModel -> TaggerWidget
+detailPane m@((^. focusedFileModel . focusedFile) -> (ConcreteTaggedFile _ hm)) =
+  hstack_
+    []
+    [ separatorLine
+    , detailPaneTagsWidget
+    ]
+ where
+  detailPaneTagsWidget =
+    withStyleBasic
+      [paddingR 20]
+      $ vstack_
+        []
+        [ filePathWidget
+        , separatorLine
+        , vscroll_ [wheelRate 50] $
+            vstack
+              [ metaLeaves
+                  ( L.sortOn (descriptor . concreteTagDescriptor)
+                      . filter
+                        ( \x ->
+                            HM.metaMember x hm
+                              && not (HM.infraMember x hm)
+                        )
+                      . HM.keys
+                      $ hm
+                  )
+              , spacer_ [resizeFactor (-1)]
+              , nullMemberLeaves
+                  ( L.sortOn (descriptor . concreteTagDescriptor)
+                      . filter
+                        ( \x ->
+                            not (HM.metaMember x hm)
+                              && not (HM.infraMember x hm)
+                        )
+                      . HM.keys
+                      $ hm
+                  )
+              , deleteTagZone
+              ]
+        ]
+   where
+    filePathWidget :: TaggerWidget
+    filePathWidget =
+      hstack_
+        []
+        [ withNodeVisible
+            ( focusedFileDefaultRecordKey
+                /= (fileId . concreteTaggedFile $ m ^. focusedFileModel . focusedFile)
+            )
+            $ styledButton_
+              [resizeFactor (-1)]
+              "Rename"
+              ( DoFocusedFileEvent
+                  (ToggleFocusedFilePaneVisibility fileRenameModeVis)
+              )
+        , zstack_
+            []
+            [ withNodeVisible (not isFileRenameMode) $
+                label_
+                  (filePath . concreteTaggedFile $m ^. focusedFileModel . focusedFile)
+                  [resizeFactor (-1)]
+            , withNodeVisible isFileRenameMode
+                . keystroke_
+                  [
+                    ( "Enter"
+                    , DoFileSelectionEvent
+                        . RenameFile
+                        $ ( fileId . concreteTaggedFile $
+                              m ^. focusedFileModel . focusedFile
+                          )
+                    )
+                  ]
+                  []
+                $ textField_
+                  ( fileSelectionModel
+                      . fileSelectionInfoMap
+                      . fileInfoAt
+                        ( fromIntegral
+                            . fileId
+                            . concreteTaggedFile
+                            $ m ^. focusedFileModel . focusedFile
+                        )
+                      . fileInfoRenameText
+                  )
+                  []
+            ]
+        ]
+     where
+      fileRenameModeVis = "file-rename"
+      isFileRenameMode =
+        (m ^. focusedFileModel . focusedFileVis)
+          `hasVis` VisibilityLabel fileRenameModeVis
+    nullMemberLeaves members =
+      withStyleBasic [borderB 1 black]
+        . vstack_ []
+        $ ( \ct@(ConcreteTag tk (Descriptor _ dp) _) ->
+              subTagDropTarget tk
+                . box_ [alignLeft, alignTop]
+                . draggable ct
+                $ label dp
+          )
+          <$> members
+    metaLeaves :: [ConcreteTag] -> TaggerWidget
+    metaLeaves members =
+      vstack_ [] . L.intersperse spacer $
+        (flip metaLeaf hm <$> members)
+     where
+      metaLeaf l@(ConcreteTag tk (Descriptor _ dp) _) hmap =
+        let subtags =
+              L.sortOn (descriptor . concreteTagDescriptor)
+                . HS.toList
+                $ HM.find l hmap
+         in if null subtags
+              then
+                subTagDropTarget tk . box_ [alignLeft, alignTop]
+                  . draggable l
+                  $ label dp
+              else
+                vstack_
+                  []
+                  [ hstack_
+                      []
+                      [ subTagDropTarget tk
+                          . box_ [alignLeft, alignTop]
+                          . draggable l
+                          . withStyleBasic [textColor yuiBlue]
+                          $ label dp
+                      , spacer
+                      , label "{"
+                      ]
+                  , hstack_
+                      []
+                      [ metaTagLeafSpacer
+                      , box_ [alignLeft, alignTop] $
+                          vstack
+                            ( flip metaLeaf hmap
+                                <$> subtags
+                            )
+                      ]
+                  , label "}"
+                  ]
+       where
+        metaTagLeafSpacer = spacer_ [width 20]
+    deleteTagZone :: TaggerWidget
+    deleteTagZone =
+      dropTarget_
+        (DoFocusedFileEvent . DeleteTag . concreteTagId)
+        [dropTargetStyle [border 1 yuiRed]]
+        . flip styleHoverSet []
+        . withStyleBasic [bgColor yuiLightPeach, border 1 yuiPeach]
+        $ buttonD_ "Delete" [resizeFactor (-1)]
+    subTagDropTarget tk =
+      dropTarget_
+        (\(Descriptor dk _) -> DoFocusedFileEvent (TagFile dk (Just tk)))
+        [dropTargetStyle [border 1 yuiBlue]]
+        . dropTarget_
+          ( \ct ->
+              DoFocusedFileEvent
+                (MoveTag ct (Just tk))
+          )
+          [dropTargetStyle [border 1 yuiRed]]
