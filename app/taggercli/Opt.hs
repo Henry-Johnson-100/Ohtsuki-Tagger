9d2a8bb65bb6f8b00eddf4050d26c39b46e20d51
monax <>
2022-09-21 15:09:44 +0000

    Wrote the functions that run an audit for missing files and unused descriptor trees. Largely unvalidated but they seem to work on first glance.
    FossilOrigin-Name: 285b8ece739fbbaadb019092e17609914faf5d8fe1e847181b73310902ed22c8

--- app/taggercli/Opt.hs
+++ app/taggercli/Opt.hs
@@ -5,26 +5,90 @@ module Opt (
 ) where
 
 import Control.Lens
+import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
 import Control.Monad.Trans.Reader
+import Control.Monad.Trans.State.Strict
+import qualified Data.Foldable as F
+import Data.HashSet (HashSet)
+import qualified Data.HashSet as HS
+import qualified Data.IntMap.Strict as IM
+import qualified Data.OccurrenceMap as OM
+import qualified Data.Text as T
 import Database.Tagger
 import Opt.Data
 import Opt.Data.Lens
-import Opt.Parser
+import System.Directory
 
 auditDatabase :: TaggedConnection -> IO TaggerDBAudit
-auditDatabase tc = flip runReaderT tc $ 
-  mconcat <$> sequence [findMissingFiles, findUnusedDescriptorTrees]
+auditDatabase tc =
+  flip runReaderT tc $
+    mconcat <$> sequence [findMissingFiles, findUnusedDescriptorTrees]
 
+{- |
+ Reports all files that do not exist or are impossible to find from the current
+ directory.
+-}
 findMissingFiles ::
   ReaderT
     TaggedConnection
     IO
     TaggerDBAudit
-findMissingFiles = undefined
+findMissingFiles = do
+  tc <- ask
+  allDBFiles <- lift $ allFiles tc
+  allMissingFiles <-
+    filterM
+      ( lift
+          . fmap not
+          . doesFileExist
+          . T.unpack
+          . filePath
+      )
+      allDBFiles
+  return $ mempty & missingFiles .~ allMissingFiles
 
+{- |
+ Reports all Descriptors which are not applied to any file
+ nor are any descriptors infra to them.
+ Signifying that they are safe to delete from the database.
+-}
 findUnusedDescriptorTrees ::
   ReaderT
     TaggedConnection
     IO
     TaggerDBAudit
-findUnusedDescriptorTrees = undefined
+findUnusedDescriptorTrees = do
+  tc <- ask
+  allDBDescriptors <- lift (allDescriptors tc)
+  unusedDescriptorTreeList <-
+    F.toList
+      <$> execStateT (scanDBDescriptorSet allDBDescriptors) mempty
+  return $ mempty & unusedDescriptorTrees .~ unusedDescriptorTreeList
+ where
+  -- Treats the state as an accumulator as it traverses the given list.
+  scanDBDescriptorSet ::
+    [Descriptor] ->
+    StateT
+      (HashSet Descriptor)
+      (ReaderT TaggedConnection IO)
+      ()
+  scanDBDescriptorSet =
+    mapM_ mutateIfUnused
+   where
+    mutateIfUnused ::
+      Descriptor ->
+      StateT (HashSet Descriptor) (ReaderT TaggedConnection IO) ()
+    mutateIfUnused d@(Descriptor dk _) = do
+      tc <- lift ask
+      infraDescriptors <- liftIO $ getAllInfra dk tc
+      infraOccurrences <-
+        IM.foldl' (+) 0
+          . OM.occurrenceMap
+          <$> liftIO
+            ( getTagOccurrencesByDescriptorKeys
+                (descriptorId <$> infraDescriptors)
+                tc
+            )
+      when (infraOccurrences <= 0) (modify (HS.insert d))
