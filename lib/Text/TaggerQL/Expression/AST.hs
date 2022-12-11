22284cdea067957ebded1a77324462ee4f9e2d5e
monax <>
2022-12-11 23:57:26 +0000

    Updating some documentation in the Expression modules
    FossilOrigin-Name: 42aa3d29fdc3d2f1af98de88fa818c19b0e0c61534ce8afe916f1f340e78e455

--- lib/Text/TaggerQL/Expression/AST.hs
+++ lib/Text/TaggerQL/Expression/AST.hs
@@ -4,6 +4,13 @@
 
 {-# HLINT ignore "Use newtype instead of data" #-}
 
+{- |
+Module      : Text.TaggerQL.Expression.AST
+Description : The syntax tree for the TaggerQL query language.
+
+License     : GPL-3
+Maintainer  : monawasensei@gmail.com
+-}
 module Text.TaggerQL.Expression.AST (
   TagTerm (..),
   FileTerm (..),
@@ -15,22 +22,54 @@ import Data.String (IsString)
 import Data.Tagger (SetOp)
 import Data.Text (Text)
 
+{- |
+ Data structure representing search terms over the set of 'Descriptor`.
+-}
 data TagTerm
-  = DescriptorTerm Text
-  | MetaDescriptorTerm Text
+  = -- | Corresponds to the set of 'Descirptor` that matches the given 'Text`
+    DescriptorTerm Text
+  | -- | Corresponds to the set of 'Descriptor` that are infra to the 'Descriptor`
+    -- matching the given 'Text`, including the matches themselves.
+    MetaDescriptorTerm Text
   deriving (Show, Eq)
 
-newtype FileTerm = FileTerm Text deriving (Show, Eq, Semigroup, Monoid, IsString)
+{- |
+ Corresponds to a search term over a filepath.
+-}
+newtype FileTerm
+  = FileTerm Text
+  deriving (Show, Eq, Semigroup, Monoid, IsString)
 
+{- |
+ A 'SubExpression` is a structure that defines a set of 'Tag` that is some
+ subset of the set of all 'Tag` in the database.
+
+ A 'SubExpression` is then used:
+  * as an operand in another 'SubExpression`
+  * to compute a set of 'File` in an 'Expression`
+  * to tag a single 'File` with the set of 'Tag` that the 'SubExpression` represents.
+-}
 data SubExpression
   = SubTag TagTerm
   | SubBinary SubExpression SetOp SubExpression
   | SubExpression TagTerm SubExpression
   deriving (Show, Eq)
 
+{- |
+ An 'Expression` is a structure that defines a set of 'File` that is some subset of
+ the set of all 'File` in the database.
+
+ An 'Expression` is a complete TaggerQL query.
+-}
 data Expression
   = FileTermValue FileTerm
   | TagTermValue TagTerm
-  | TagExpression TagTerm SubExpression
-  | Binary Expression SetOp Expression
+  | -- | Constructs a 'Tag` set from the given 'TagTerm`
+    -- that serves as the inital environment for the given 'SubExpression`.
+    --
+    -- Essentially, defines the set of 'File` where 'SubExpression` are subtags
+    -- of any 'Tag` appearing in the set defined by the 'TagTerm`.
+    TagExpression TagTerm SubExpression
+  | -- | A binary operation over two 'Expression`
+    Binary Expression SetOp Expression
   deriving (Show, Eq)
