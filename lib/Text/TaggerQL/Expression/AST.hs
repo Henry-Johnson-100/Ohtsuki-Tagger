d4ec6706f1ffa9daeec7c9c4ceaec63492a2d6a1
monax <>
2023-01-04 14:23:46 +0000

    Added a Ring class for simple identities and combining expressions with the appropriate setop easily.
    FossilOrigin-Name: 2b06e70466f5c69169b68103eda64c3837f18b75a8ec29224943430532909d96

--- lib/Text/TaggerQL/Expression/AST.hs
+++ lib/Text/TaggerQL/Expression/AST.hs
@@ -1,6 +1,8 @@
 {-# HLINT ignore "Use newtype instead of data" #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE StrictData #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
@@ -18,10 +20,13 @@ module Text.TaggerQL.Expression.AST (
   TagTermExtension (..),
   SubExpression (..),
   Expression (..),
+
+  -- * Classes
+  Ring (..),
 ) where
 
 import Data.String (IsString)
-import Data.Tagger (SetOp)
+import Data.Tagger (SetOp (..))
 import Data.Text (Text)
 
 {- |
@@ -82,3 +87,50 @@ data Expression
 data BinaryOperation a = BinaryOperation a SetOp a deriving (Show, Eq, Functor)
 
 data TagTermExtension a = TagTermExtension TagTerm a deriving (Show, Eq, Functor)
+
+class Ring r where
+  -- | Identity over '(<+>)`
+  aid :: r
+
+  -- | Identity over '(<^>)`
+  mid :: r
+
+  -- | An associative operation
+  (<+>) :: r -> r -> r
+
+  -- | An associative operation
+  (<^>) :: r -> r -> r
+
+  -- | The inverse of '(<+>)`
+  (<->) :: r -> r -> r
+
+instance Ring SubExpression where
+  mid :: SubExpression
+  mid = SubTag . DescriptorTerm $ "%"
+  aid :: SubExpression
+  aid = BinarySubExpression $ BinaryOperation mid Difference mid
+  (<+>) :: SubExpression -> SubExpression -> SubExpression
+  x <+> y = BinarySubExpression $ BinaryOperation x Union y
+  (<^>) :: SubExpression -> SubExpression -> SubExpression
+  x <^> y = BinarySubExpression $ BinaryOperation x Intersect y
+  (<->) :: SubExpression -> SubExpression -> SubExpression
+  x <-> y = BinarySubExpression $ BinaryOperation x Difference y
+
+instance Ring Expression where
+  mid :: Expression
+  mid = FileTermValue "%"
+  aid :: Expression
+  aid =
+    let dUniverse = TagTermValue . DescriptorTerm $ "%"
+     in BinaryExpression
+          ( BinaryOperation
+              (BinaryExpression (BinaryOperation mid Difference dUniverse))
+              Intersect
+              dUniverse
+          )
+  (<+>) :: Expression -> Expression -> Expression
+  x <+> y = BinaryExpression $ BinaryOperation x Union y
+  (<^>) :: Expression -> Expression -> Expression
+  x <^> y = BinaryExpression $ BinaryOperation x Intersect y
+  (<->) :: Expression -> Expression -> Expression
+  x <-> y = BinaryExpression $ BinaryOperation x Difference y
