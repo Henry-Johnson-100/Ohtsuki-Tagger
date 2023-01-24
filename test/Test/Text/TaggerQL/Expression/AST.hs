1e08b12becf04b875d4f452a0b0082b860d08327
monax <>
2023-01-24 14:13:30 +0000

    The auto formatter just decided to indent everything to 4 spaces in the test file for some reason
    FossilOrigin-Name: 1101e027ab92b9ba31f3357b6ccda300b122836ae37cc59126389b7f3921da3d

--- test/Test/Text/TaggerQL/Expression/AST.hs
+++ test/Test/Text/TaggerQL/Expression/AST.hs
@@ -4,7 +4,7 @@
 {-# OPTIONS_GHC -Wno-typed-holes #-}
 
 module Test.Text.TaggerQL.Expression.AST (
-  astTests,
+    astTests,
 ) where
 
 import Control.Monad ((>=>))
@@ -15,89 +15,89 @@ import Text.TaggerQL.Expression.AST
 
 -- change this at some point
 instance Arbitrary Pattern where
-  arbitrary :: Gen Pattern
-  arbitrary = Pattern . T.pack <$> suchThat arbitrary (not . null)
+    arbitrary :: Gen Pattern
+    arbitrary = Pattern . T.pack <$> suchThat arbitrary (not . null)
 
 instance Arbitrary a => Arbitrary (DTerm a) where
-  arbitrary :: Arbitrary a => Gen (DTerm a)
-  arbitrary = do
-    constructor <- oneof [pure DTerm, pure DMetaTerm]
-    value <- arbitrary
-    pure . constructor $ value
+    arbitrary :: Arbitrary a => Gen (DTerm a)
+    arbitrary = do
+        constructor <- oneof [pure DTerm, pure DMetaTerm]
+        value <- arbitrary
+        pure . constructor $ value
 
 instance Arbitrary a => Arbitrary (RingExpression a) where
-  arbitrary :: Arbitrary a => Gen (RingExpression a)
-  arbitrary = do
-    let ringOperation =
-          oneof [pure (:+), pure (:*), pure (:-)] <*> arbitrary <*> arbitrary
-    oneof [Ring <$> arbitrary, ringOperation]
+    arbitrary :: Arbitrary a => Gen (RingExpression a)
+    arbitrary = do
+        let ringOperation =
+                oneof [pure (:+), pure (:*), pure (:-)] <*> arbitrary <*> arbitrary
+        oneof [Ring <$> arbitrary, ringOperation]
 
 astTests :: TestTree
 astTests =
-  testGroup
-    "AST Tests"
-    [astProperties]
+    testGroup
+        "AST Tests"
+        [astProperties]
 
 astProperties :: TestTree
 astProperties =
-  testGroup
-    "Expression Properties"
-    [ testGroup
-        "RingExpression Properties"
-        [ testProperty
-            "Left Monad Identity"
-            ( do
-                f <- arbitrary :: Gen (Int -> RingExpression Int)
-                i <- arbitrary
-                let testProp = (pure i >>= f) == f i
-                pure testProp
-            )
-        , testProperty
-            "Right Monad Identity"
-            ( do
-                re <- arbitrary :: Gen (RingExpression Int)
-                let testProp = (re >>= pure) == re
-                pure testProp
-            )
-        , localOption (QuickCheckTests 30) $
-            testProperty
-              "Kleisli Arrow Associativity"
-              ( do
-                  f <- arbitrary :: Gen (Int -> RingExpression Int)
-                  g <- arbitrary :: Gen (Int -> RingExpression Int)
-                  h <- arbitrary :: Gen (Int -> RingExpression Int)
-                  dt <- arbitrary :: Gen Int
-                  let testProp = (f >=> (g >=> h)) dt == ((f >=> g) >=> h) dt
-                      propTerminates = within (500 * (10 ^ (3 :: Integer))) testProp
-                  pure $ propTerminates .||. Discard
-              )
+    testGroup
+        "Expression Properties"
+        [ testGroup
+            "RingExpression Properties"
+            [ testProperty
+                "Left Monad Identity"
+                ( do
+                    f <- arbitrary :: Gen (Int -> RingExpression Int)
+                    i <- arbitrary
+                    let testProp = (pure i >>= f) == f i
+                    pure testProp
+                )
+            , testProperty
+                "Right Monad Identity"
+                ( do
+                    re <- arbitrary :: Gen (RingExpression Int)
+                    let testProp = (re >>= pure) == re
+                    pure testProp
+                )
+            , localOption (QuickCheckTests 30) $
+                testProperty
+                    "Kleisli Arrow Associativity"
+                    ( do
+                        f <- arbitrary :: Gen (Int -> RingExpression Int)
+                        g <- arbitrary :: Gen (Int -> RingExpression Int)
+                        h <- arbitrary :: Gen (Int -> RingExpression Int)
+                        dt <- arbitrary :: Gen Int
+                        let testProp = (f >=> (g >=> h)) dt == ((f >=> g) >=> h) dt
+                            propTerminates = within (500 * (10 ^ (3 :: Integer))) testProp
+                        pure $ propTerminates .||. Discard
+                    )
+            ]
+        , testGroup
+            "DTerm properties"
+            [ testProperty
+                "Left Monad Identity"
+                ( do
+                    f <- arbitrary :: Gen (Int -> DTerm Int)
+                    i <- arbitrary :: Gen Int
+                    pure $ (pure i >>= f) == f i
+                )
+            , testProperty
+                "Right Monad Identity"
+                ( do
+                    dt <- arbitrary :: Gen (DTerm Int)
+                    pure $ (dt >>= pure) == dt
+                )
+            , localOption (QuickCheckTests 30) $
+                testProperty
+                    "Kleisli Arrow Associativity"
+                    ( do
+                        f <- arbitrary :: Gen (Int -> DTerm Int)
+                        g <- arbitrary :: Gen (Int -> DTerm Int)
+                        h <- arbitrary :: Gen (Int -> DTerm Int)
+                        dt <- arbitrary :: Gen Int
+                        let testProp = (f >=> (g >=> h)) dt == ((f >=> g) >=> h) dt
+                            propTerminates = within (500 * (10 ^ (3 :: Integer))) testProp
+                        pure $ propTerminates .||. Discard
+                    )
+            ]
         ]
-    , testGroup
-        "DTerm properties"
-        [ testProperty
-            "Left Monad Identity"
-            ( do
-                f <- arbitrary :: Gen (Int -> DTerm Int)
-                i <- arbitrary :: Gen Int
-                pure $ (pure i >>= f) == f i
-            )
-        , testProperty
-            "Right Monad Identity"
-            ( do
-                dt <- arbitrary :: Gen (DTerm Int)
-                pure $ (dt >>= pure) == dt
-            )
-        , localOption (QuickCheckTests 30) $
-            testProperty
-              "Kleisli Arrow Associativity"
-              ( do
-                  f <- arbitrary :: Gen (Int -> DTerm Int)
-                  g <- arbitrary :: Gen (Int -> DTerm Int)
-                  h <- arbitrary :: Gen (Int -> DTerm Int)
-                  dt <- arbitrary :: Gen Int
-                  let testProp = (f >=> (g >=> h)) dt == ((f >=> g) >=> h) dt
-                      propTerminates = within (500 * (10 ^ (3 :: Integer))) testProp
-                  pure $ propTerminates .||. Discard
-              )
-        ]
-    ]
