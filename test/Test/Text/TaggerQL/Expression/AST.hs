a4a0b279c3603bc1c28ded646a76e49523e58a49
monax <>
2023-01-24 15:08:08 +0000

    Wrote an Arbitrary instance for TagExpressions and rewrote the RingExpression instance to use a sized parameter.
    Also removed the _ .||. Discard tests in the Kleisli associativity properties because
    I suspect that that pattern allows every test to pass, regardless of how it failed.
    
    So I replaced the randomly generated functions with simple Int functions and opted
    to just increase the test size. I will come back to this to see if there is a 
    better way to generate non-terminating arbitrary functions.
    
    FossilOrigin-Name: d58692aca2ab21f85ef7afc30725913492387d3e680ec3115300f916057ecb68

--- test/Test/Text/TaggerQL/Expression/AST.hs
+++ test/Test/Text/TaggerQL/Expression/AST.hs
@@ -28,10 +28,16 @@ instance Arbitrary a => Arbitrary (DTerm a) where
 
 instance Arbitrary a => Arbitrary (RingExpression a) where
     arbitrary :: Arbitrary a => Gen (RingExpression a)
-    arbitrary = do
-        let ringOperation =
-                oneof [pure (:+), pure (:*), pure (:-)] <*> arbitrary <*> arbitrary
-        oneof [Ring <$> arbitrary, ringOperation]
+    arbitrary = sized sizedRing
+      where
+        sizedRing n
+            | n <= 0 = Ring <$> arbitrary
+            | otherwise =
+                oneof
+                    [ (:+) <$> sizedRing (n `div` 2) <*> sizedRing (n `div` 2)
+                    , (:*) <$> sizedRing (n `div` 2) <*> sizedRing (n `div` 2)
+                    , (:-) <$> sizedRing (n `div` 2) <*> sizedRing (n `div` 2)
+                    ]
 
 instance Arbitrary a => Arbitrary (MagmaExpression a) where
     arbitrary :: Arbitrary a => Gen (MagmaExpression a)
@@ -40,6 +46,17 @@ instance Arbitrary a => Arbitrary (MagmaExpression a) where
         case xs' of
             (x : xs) -> pure $ F.foldl' over (pure x) xs
             _emptyList -> Magma <$> arbitrary
+
+instance Arbitrary a => Arbitrary (TagExpression a) where
+    arbitrary :: Arbitrary a => Gen (TagExpression a)
+    arbitrary = sized sizedExpr
+      where
+        sizedExpr n
+            | n <= 0 = TagValue <$> arbitrary
+            | otherwise =
+                let tagRing = TagRing <$> resize (n `div` 2) arbitrary
+                    tagMagma = TagMagma <$> resize (n `div` 2) arbitrary
+                 in oneof [tagRing, tagMagma]
 
 astTests :: TestTree
 astTests =
@@ -68,18 +85,16 @@ astProperties =
                     let testProp = (re >>= pure) == re
                     pure testProp
                 )
-            , localOption (QuickCheckTests 30) $
-                testProperty
-                    "Kleisli Arrow Associativity"
-                    ( do
-                        f <- arbitrary :: Gen (Int -> RingExpression Int)
-                        g <- arbitrary :: Gen (Int -> RingExpression Int)
-                        h <- arbitrary :: Gen (Int -> RingExpression Int)
-                        dt <- arbitrary :: Gen Int
-                        let testProp = (f >=> (g >=> h)) dt == ((f >=> g) >=> h) dt
-                            propTerminates = within (500 * (10 ^ (3 :: Integer))) testProp
-                        pure $ propTerminates .||. Discard
-                    )
+            , testProperty
+                "Kleisli Arrow Associativity"
+                ( do
+                    let f n = pure (n `div` 2) :+ pure (n * n)
+                        g n = pure (n - (n * (-3))) :* pure n
+                        h n = pure n :- (pure (n + 23) :* pure (n * 1001))
+                    dt <- arbitrary :: Gen Int
+                    let testProp = (f >=> (g >=> h)) dt == ((f >=> g) >=> h) dt
+                    pure testProp
+                )
             ]
         , testGroup
             "MagmaExpression Properties"
@@ -98,18 +113,17 @@ astProperties =
                     let testProp = (re >>= pure) == re
                     pure testProp
                 )
-            , localOption (QuickCheckTests 30) $
-                testProperty
-                    "Kleisli Arrow Associativity"
-                    ( do
-                        f <- arbitrary :: Gen (Int -> MagmaExpression Int)
-                        g <- arbitrary :: Gen (Int -> MagmaExpression Int)
-                        h <- arbitrary :: Gen (Int -> MagmaExpression Int)
-                        dt <- arbitrary :: Gen Int
-                        let testProp = (f >=> (g >=> h)) dt == ((f >=> g) >=> h) dt
-                            propTerminates = within (500 * (10 ^ (3 :: Integer))) testProp
-                        pure $ propTerminates .||. Discard
-                    )
+            , testProperty
+                "Kleisli Arrow Associativity"
+                ( do
+                    f <- arbitrary :: Gen (Int -> MagmaExpression Int)
+                    g <- arbitrary :: Gen (Int -> MagmaExpression Int)
+                    h <- arbitrary :: Gen (Int -> MagmaExpression Int)
+                    dt <- arbitrary :: Gen Int
+                    let testProp = (f >=> (g >=> h)) dt == ((f >=> g) >=> h) dt
+                        propTerminates = within (500 * (10 ^ (3 :: Integer))) testProp
+                    pure propTerminates
+                )
             ]
         , testGroup
             "DTerm properties"
@@ -126,17 +140,15 @@ astProperties =
                     dt <- arbitrary :: Gen (DTerm Int)
                     pure $ (dt >>= pure) == dt
                 )
-            , localOption (QuickCheckTests 30) $
-                testProperty
-                    "Kleisli Arrow Associativity"
-                    ( do
-                        f <- arbitrary :: Gen (Int -> DTerm Int)
-                        g <- arbitrary :: Gen (Int -> DTerm Int)
-                        h <- arbitrary :: Gen (Int -> DTerm Int)
-                        dt <- arbitrary :: Gen Int
-                        let testProp = (f >=> (g >=> h)) dt == ((f >=> g) >=> h) dt
-                            propTerminates = within (100 * (10 ^ (3 :: Integer))) testProp
-                        pure $ propTerminates .||. Discard
-                    )
+            , testProperty
+                "Kleisli Arrow Associativity"
+                ( do
+                    let f n = pure (n `div` 2)
+                        g n = DTerm (n - (n * (-3)))
+                        h n = DMetaTerm (n * 1001)
+                    dt <- arbitrary :: Gen Int
+                    let testProp = (f >=> (g >=> h)) dt == ((f >=> g) >=> h) dt
+                    pure testProp
+                )
             ]
         ]
