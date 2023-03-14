cffc4306d9582f0bf5d21867b67aea1e57e26141
monax <>
2023-03-14 13:39:59 +0000

    Organize theme code
    FossilOrigin-Name: d289ddef38e0d561ead0d26ea1cfb845a3f0d8f1517748caf061424a1c3dddf5

--- app/tagger/Interface/Theme.hs
+++ app/tagger/Interface/Theme.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -6,11 +5,11 @@ module Interface.Theme (
   module Interface.Theme,
 ) where
 
-import Config
-import Control.Monad
-import Control.Monad.Trans.Maybe
+import Config (TaggerConfig (taggerConfigScaleFactor), getOptConf)
+import Control.Monad (join)
+import Control.Monad.Trans.Maybe (MaybeT (runMaybeT))
 import Data.Event (TaggerEvent (CloseConnection))
-import Data.Maybe
+import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
 import Monomer (
   AppConfig,
@@ -58,12 +57,18 @@ import System.Directory (makeAbsolute)
 
 themeConfig :: IO [AppConfig TaggerEvent]
 themeConfig = do
-  defaultThinFont <- T.pack <$> (makeAbsolute =<< PT.getDataFileName "resources/iosevka_thin.ttf")
+  defaultThinFont <-
+    T.pack
+      <$> (makeAbsolute =<< PT.getDataFileName "resources/iosevka_thin.ttf")
   defaultRegularFont <-
     T.pack
       <$> (makeAbsolute =<< PT.getDataFileName "resources/iosevka_regular.ttf")
-  defaultBoldFont <- T.pack <$> (makeAbsolute =<< PT.getDataFileName "resources/iosevka_bold.ttf")
-  dataIcon <- T.pack <$> (makeAbsolute =<< PT.getDataFileName "resources/Yui_signature_SS.bmp")
+  defaultBoldFont <-
+    T.pack
+      <$> (makeAbsolute =<< PT.getDataFileName "resources/iosevka_bold.ttf")
+  dataIcon <-
+    T.pack
+      <$> (makeAbsolute =<< PT.getDataFileName "resources/Yui_signature_SS.bmp")
   maybeDefaultScaleFactor <- join <$> runMaybeT (taggerConfigScaleFactor <$> getOptConf)
   return
     [ appWindowTitle "Tagger"
